171a41a4d2517fc3a20bae9d94918b17bf811ed6
Akihiro Takai <at.sisy@gmail.com>
2017-05-26 07:05:19 +0900

    開発環境移行のための一時的コミット

---
+++ core/Images.java
@@ -0,0 +1,19 @@
+package core;
+
+import javafx.scene.image.Image;
+
+/**
+ * Created by Akihiro on 2017/05/21.
+ */
+public class Images {
+
+    private Image[] images;
+
+    public Images(){
+        images = new Image[20];
+
+
+
+    }
+
+}
